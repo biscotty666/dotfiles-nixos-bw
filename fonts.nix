181a7153c2708c8a212408c788a99b53ed4d8b5b
biscotty666 <biscotty666@gmail.com>
2024-05-02 22:17:18 -0600

    Added fonts

---
+++ fonts.nix
@@ -0,0 +1,21 @@
+{ config, pkgs, ... }:
+
+{
+  fonts.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+    liberation_ttf
+    fira-code
+    fira-code-symbols
+    mplus-outline-fonts.githubRelease
+    dina-font
+    proggyfonts
+    nerdfonts
+#     (nerdfonts.override { fonts = [ 
+#       "FantasqueSansMono" 
+#       "FiraCode"
+#       "DroidSansMono"
+#     ]; })
+  ];
+}
