25c9ed5ed7322a1f9010553c19de37c4a56592b1
biscotty666 <biscotty666@gmail.com>
2024-06-14 13:18:51 -0600

    Add packages for haskell gtk and cabal

--- biscotty/haskell.nix
+++ biscotty/haskell.nix
@@ -8,6 +8,8 @@
     haskellPackages.streaming-bytestring
     haskellPackages.gi-gtk
     haskellPackages.gtk3
+    haskellPackages.Cabal_3_10_3_0
+    haskellPackages.cabal-install
     gtk3
     lua51Packages.haskell-tools-nvim
   ];
