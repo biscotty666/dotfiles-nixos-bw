f68895203c54bae24ef4a6ff8ef7650201d1a2bc
biscotty666 <biscotty666@gmail.com>
2024-10-23 09:46:07 -0600

    Added language servers to neovim;

--- biscotty/neovim/plugins/lsp.nix
+++ biscotty/neovim/plugins/lsp.nix
@@ -24,8 +24,24 @@
 
         servers = {
           clangd.enable = true;
+          html.enable = true;
+          htmx.enable = true;
+          jsonls.enable = true;
+          ltex.enable = true;
+          nixd.enable = true;
+          quick_lint_js.enable = true;
           lua_ls.enable = true;
           texlab.enable = true;
+          cssls.enable = true;
+          emmet_ls.enable = true;
+          jedi_language_server.enable = true;
+          markdown_oxide.enable = true;
+          tailwindcss.enable = true;
+          ghcide.enable = true;
+          hls = {
+            enable = true;
+            installGhc = true;
+          };
         };
       };
     };
