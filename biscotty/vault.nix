8af726d34ee057d36253e78ad44702ddf1f30f77
biscotty666 <biscotty666@gmail.com>
2024-10-30 10:44:02 -0600

    Improved unlock-vaults service;
    Testing for empty directory

--- biscotty/vault.nix
+++ biscotty/vault.nix
@@ -12,8 +12,20 @@
     Service = {
       ExecStart = "${pkgs.writeShellScript "unlock-vaults" ''
         #!/run/current-system/sw/bin/bash
-        /home/biscotty/.nix-profile/bin/gocryptfs /home/biscotty/Dropbox/DropVault /home/biscotty/DropOpen -passfile /home/biscotty/.local/share/gocrypt/secret.txt
-        /home/biscotty/.nix-profile/bin/gocryptfs /home/biscotty/DocVault /home/biscotty/Documents -passfile /home/biscotty/.local/share/gocrypt/secret.txt
+        if [ -z "$(ls -A /home/biscotty/DropOpen)" ];
+        then
+            echo "Empty Directory ";
+            /home/biscotty/.nix-profile/bin/gocryptfs /home/biscotty/Dropbox/DropVault /home/biscotty/DropOpen -passfile /home/biscotty/.local/share/gocrypt/secret.txt
+        else
+            echo "Directory isn’t empty";
+        fi
+        if [ -z "$(ls -A /home/biscotty/Documents)" ];
+        then
+            echo "Empty Directory ";
+            /home/biscotty/.nix-profile/bin/gocryptfs /home/biscotty/DocVault /home/biscotty/Documents -passfile /home/biscotty/.local/share/gocrypt/secret.txt
+        else
+            echo "Directory isn’t empty";
+        fi
               ''}";
     };
   };
