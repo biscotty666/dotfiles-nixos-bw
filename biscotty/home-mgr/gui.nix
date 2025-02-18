a4a4d63355e6fadd32df748766fea1858fce8dbd
biscotty666 <biscotty666@gmail.com>
2025-02-18 01:39:49 -0700

    Removed gimp and jupyter;

--- biscotty/home-mgr/gui.nix
+++ biscotty/home-mgr/gui.nix
@@ -5,17 +5,12 @@
     thunderbird
     floorp
     obsidian
-        #    onlyoffice-bin
     protonvpn-gui
     brave
     libreoffice
     freetube
-    jupyter
-    gimp
     vlc
     firefox
-        #inkscape
-        #    zeroadPackages.zeroad
     spotify
     discord
   ];
