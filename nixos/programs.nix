2fe657dba1d3366b4655f47daacb2a456bc3e387
biscotty666 <biscotty666@gmail.com>
2025-02-25 17:41:53 -0700

    Cleanup;

--- nixos/programs.nix
+++ nixos/programs.nix
@@ -56,12 +56,10 @@
     vim-full # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     git
     alacritty
-    #wezterm
     restic
     xdg-desktop-portal-gtk
     xdg-utils
     qemu
-    #quickemu
     mlocate
     dive
     cachix
