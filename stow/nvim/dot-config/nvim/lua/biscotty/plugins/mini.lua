2c836466920b736255ddc2fb1b14ac671210d829
biscotty666 <biscotty666@gmail.com>
2025-12-23 11:05:17 -0700

    Some nvim mods;

--- stow/nvim/dot-config/nvim/lua/biscotty/plugins/mini.lua
+++ stow/nvim/dot-config/nvim/lua/biscotty/plugins/mini.lua
@@ -64,7 +64,7 @@ return {
             -- saw surround with whitespace
             mappings = {
                 add = 'sa',            -- Add surrounding in Normal and Visual modes
-                delete = 'ds',         -- Delete surrounding
+                delete = 'sd',         -- Delete surrounding
                 find = 'sf',           -- Find surrounding (to the right)
                 find_left = 'sF',      -- Find surrounding (to the left)
                 highlight = 'sh',      -- Highlight surrounding
