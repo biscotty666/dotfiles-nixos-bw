13a46c89c479a25627ee1d9f6d2ee62d213e0d85
biscotty666 <biscotty666@gmail.com>
2026-04-14 11:50:11 -0600

    Cleaned up files;

--- configuration.nix
+++ configuration.nix
@@ -79,31 +79,6 @@
 
   zramSwap.enable = true;
 
-  # services.displayManager.gdm.enable = true;
-
-  # services.xserver.displayManager.lightdm = {
-  #   enable = true;
-  #   greeters.lomiri.enable = true;
-  #   # greeters.enso = {
-  #   #   enable = true;
-  #   #   blur = true;
-  #   # };
-  #   # greeters.slick.enable = true;
-  # };
-  # services.displayManager.dms-greeter = {
-  #   enable = true;
-  #   compositor.name = "niri";
-  # };
-  # services.xserver.displayManager.setupCommands = "
-  #   xrandr --output eDP-1 --auto --primary
-  #   xrandr --output HDMI-A-1 --auto --noprimary
-  #   ";
-  # services.displayManager.sddm = {
-  #   enable = true;
-  #   theme = "catppuccin-mocha-mauve";
-  #   wayland.enable = true;
-  #   wayland.compositor = "weston";
-  # };
   programs.ssh.askPassword = pkgs.lib.mkForce "${pkgs.kdePackages.ksshaskpass.out}/bin/ksshaskpass";
   # services.xserver.desktopManager.xfce.enable = true;
   services.desktopManager.plasma6 = {
--- nixos/dm.nix
+++ nixos/dm.nix
@@ -17,4 +17,25 @@
     wayland.enable = true;
     wayland.compositor = "weston";
   };
+
+  services.xserver.displayManager.setupCommands = "
+    xrandr --output eDP-1 --auto --primary
+    xrandr --output HDMI-A-1 --auto --noprimary
+    ";
+
+  # services.displayManager.gdm.enable = true;
+
+  # services.xserver.displayManager.lightdm = {
+  #   enable = true;
+  #   greeters.lomiri.enable = true;
+  #   # greeters.enso = {
+  #   #   enable = true;
+  #   #   blur = true;
+  #   # };
+  #   # greeters.slick.enable = true;
+  # };
+  # services.displayManager.dms-greeter = {
+  #   enable = true;
+  #   compositor.name = "niri";
+  # };
 }
