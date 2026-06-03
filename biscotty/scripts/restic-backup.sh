db5490fcb3268ed1b7303452c13fa64f0a7efa3f
biscotty666 <biscotty666@gmail.com>
2026-06-03 14:10:40 -0600

    Fixed backups of Datasets and pot;

--- biscotty/scripts/restic-backup.sh
+++ biscotty/scripts/restic-backup.sh
@@ -2,6 +2,7 @@
 #! nix-shell -i bash -p bash
 
 /run/current-system/sw/bin/restic backup -r /home/biscotty/seagate/backups/sb -p /home/biscotty/.config/restic/restpass /home/biscotty/Obsidian --tag Obsidian
+/run/current-system/sw/bin/restic backup -r /home/biscotty/seagate/backups/sb -p /home/biscotty/.config/restic/restpass /home/biscotty/Audible --tag Audible
 /run/current-system/sw/bin/restic backup -r /home/biscotty/seagate/backups/sb -p /home/biscotty/.config/restic/restpass /home/biscotty/Learning --tag Learning
 /run/current-system/sw/bin/restic backup -r /home/biscotty/seagate/backups/sb -p /home/biscotty/.config/restic/restpass /home/biscotty/Camera --tag Camera
 /run/current-system/sw/bin/restic backup -r /home/biscotty/seagate/backups/sb -p /home/biscotty/.config/restic/restpass /home/biscotty/DocVault --tag DocumentVault
@@ -9,6 +10,7 @@
 /run/current-system/sw/bin/restic backup -r /home/biscotty/seagate/backups/sb -p /home/biscotty/.config/restic/restpass /home/biscotty/.local --tag local
 /run/current-system/sw/bin/restic backup -r /home/biscotty/seagate/backups/sb -p /home/biscotty/.config/restic/restpass /home/biscotty/.config --tag config
 /run/current-system/sw/bin/restic backup -r /home/biscotty/seagate/backups/sb -p /home/biscotty/.config/restic/restpass /home/biscotty/.thunderbird --tag thunderbird
-/run/current-system/sw/bin/restic backup -r sftp:rpi:/home/biscotty/backup/pot -p /home/biscotty/.config/restic/restpass /home/biscotty/seagate/pot --tag pot
+/run/current-system/sw/bin/restic backup -r sftp:rpi:/home/biscotty/seagate/backup/pot -p /home/biscotty/.config/restic/restpass /home/biscotty/seagate/pot --tag pot
+/run/current-system/sw/bin/restic backup -r sftp:rpi:/home/biscotty/seagate/backup/pot -p /home/biscotty/.config/restic/restpass /home/biscotty/seagate/Datasets --tag Datasets
 /run/current-system/sw/bin/restic forget -r /home/biscotty/seagate/backups/sb -p /home/biscotty/.config/restic/restpass --keep-daily 4 --keep-weekly 4 --keep-monthly 12 --keep-yearly 2 --prune
-/run/current-system/sw/bin/restic forget -r sftp:rpi:/home/biscotty/backup/pot -p /home/biscotty/.config/restic/restpass --keep-daily 4 --keep-weekly 4 --keep-monthly 12 --keep-yearly 2 --prune
+/run/current-system/sw/bin/restic forget -r sftp:rpi:/home/biscotty/seagate/backup/pot -p /home/biscotty/.config/restic/restpass --keep-daily 4 --keep-weekly 4 --keep-monthly 12 --keep-yearly 2 --prune
