2888ae66f4e5d7c4e2fc6c5ced2d4cd54e74dbbf
biscotty666 <biscotty666@gmail.com>
2026-06-10 08:47:58 -0600

    Hopefully fixed backups not running;

--- biscotty/scripts/restic-backup.sh
+++ biscotty/scripts/restic-backup.sh
@@ -1,6 +1,3 @@
-#! /usr/bin/env nix-shell
-#! nix-shell -i bash -p bash
-
 /run/current-system/sw/bin/restic backup -r /home/biscotty/seagate/backups/sb -p /home/biscotty/.config/restic/restpass /home/biscotty/Obsidian --tag Obsidian
 /run/current-system/sw/bin/restic backup -r /home/biscotty/seagate/backups/sb -p /home/biscotty/.config/restic/restpass /home/biscotty/Audible --tag Audible
 /run/current-system/sw/bin/restic backup -r /home/biscotty/seagate/backups/sb -p /home/biscotty/.config/restic/restpass /home/biscotty/Learning --tag Learning
@@ -9,6 +6,7 @@
 /run/current-system/sw/bin/restic backup -r /home/biscotty/seagate/backups/sb -p /home/biscotty/.config/restic/restpass /home/biscotty/Projects --tag Projects
 /run/current-system/sw/bin/restic backup -r /home/biscotty/seagate/backups/sb -p /home/biscotty/.config/restic/restpass /home/biscotty/.local --tag local
 /run/current-system/sw/bin/restic backup -r /home/biscotty/seagate/backups/sb -p /home/biscotty/.config/restic/restpass /home/biscotty/.config --tag config
+/run/current-system/sw/bin/restic backup -r /home/biscotty/seagate/backups/sb -p /home/biscotty/.config/restic/restpass /home/biscotty/.dotfiles --tag dotfiles
 /run/current-system/sw/bin/restic backup -r /home/biscotty/seagate/backups/sb -p /home/biscotty/.config/restic/restpass /home/biscotty/.thunderbird --tag thunderbird
 /run/current-system/sw/bin/restic backup -r sftp:rpi:/home/biscotty/seagate/backup/pot -p /home/biscotty/.config/restic/restpass /home/biscotty/seagate/pot --tag pot
 /run/current-system/sw/bin/restic backup -r sftp:rpi:/home/biscotty/seagate/backup/pot -p /home/biscotty/.config/restic/restpass /home/biscotty/seagate/Datasets --tag Datasets
