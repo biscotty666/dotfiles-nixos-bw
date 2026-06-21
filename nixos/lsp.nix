4c887a6e31b3b5dfb55c971df02dd4d46ea97538
biscotty666 <biscotty666@gmail.com>
2026-06-21 09:53:58 -0600

    Messing with nvim;

--- nixos/lsp.nix
+++ nixos/lsp.nix
@@ -3,15 +3,10 @@
 {
 
   environment.systemPackages = with pkgs; [
-#    kdePackages.fcitx5-chinese-addons
-    nuspell
-    hunspellDicts.en_US
-    hunspellDicts.es_ANY
-    hunspellDicts.fr-any
-    aspell
-    aspellDicts.fr
-    aspellDicts.es
-    aspellDicts.en
+    markdown-oxide
+    ruff
+    rPackages.lintr
+    rPackages.languageserver
  ];
 
 }
