7a7cdf67c52622aee58981b2ca69c4db198c148b
biscotty666 <biscotty666@gmail.com>
2026-07-09 13:47:34 -0600

    Added quarto.lua for neovim;

--- nixos/lsp.nix
+++ nixos/lsp.nix
@@ -1,12 +1,14 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
-
   environment.systemPackages = with pkgs; [
     markdown-oxide
     ruff
     rPackages.lintr
     rPackages.languageserver
- ];
+    typescript-language-server
+    luaPackages.tree-sitter-cli
+    nil
+  ];
 
 }
