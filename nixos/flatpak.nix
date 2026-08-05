109bc5cbc1fc95a96dbb5544818642a4f52f4911
biscotty666 <biscotty666@gmail.com>
2026-08-04 22:50:40 -0600

    Added flatpak's openshot;
    Nixos's openshot-qt produces videos that freeze

--- nixos/flatpak.nix
+++ nixos/flatpak.nix
@@ -1,10 +1,11 @@
-{ config, ... }:
+{ ... }:
 
 {
   services.flatpak = {
     enable = true;
     packages = [
       "app.zen_browser.zen"
+      "org.openshot.OpenShot"
     ];
     update.auto = {
       enable = true;
