16b9df7fa8d30bd8671b0cde5acc0e8e6ab7957f
biscotty666 <biscotty666@gmail.com>
2026-09-09 08:41:48 -0600

    Added cuda substituter;

--- nixos/cachix.nix
+++ nixos/cachix.nix
@@ -1,17 +1,19 @@
 {
   nix.settings = {
     substituters = [
-      https://cache.nixos.org
-      https://cache.iog.io
-      https://cache.nixos.org/
-      https://cuda-maintainers.cachix.org
-      https://nix-community.cachix.org
+      "https://cache.nixos.org"
+      "https://cache.iog.io"
+      "https://cache.nixos.org/"
+      "https://cuda-maintainers.cachix.org"
+      "https://nix-community.cachix.org"
+      "https://cache.nixos-cuda.org"
     ];
     trusted-public-keys = [
-      cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=
-      hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=
-      cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E=
-      nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
     ];
     extra-substituters = [ "https://yazi.cachix.org" ];
     extra-trusted-public-keys = [ "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k=" ];
