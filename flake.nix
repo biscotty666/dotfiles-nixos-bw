416b019bbca5e5bdeacc1da824265c877843d341
biscotty666 <biscotty666@gmail.com>
2026-09-22 19:57:42 -0600

    Switched input.nixpkgs.url to channels;

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "Biscotty's Workshop";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     # sops-nix = {
